e62ea48018dadecc06f24632f4a6a739037191be
Dipesh B C <bcdipeshwork@gmail.com>
2025-05-14 20:27:11 -0400

    build: setup shadcn ui component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Card, CardHeader } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+
 function App() {
-  return <>Hello There</>;
+  return (
+    <Card>
+      <CardHeader>Hello</CardHeader>
+      <Button>Hello again</Button>
+    </Card>
+  );
 }
 
 export default App;
